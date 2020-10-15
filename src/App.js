501ce546065985cd01ab3038cd65497977a228e5
David Odesola <faithodesola@gmail.com>
2020-10-15 22:21:39 +0800

    Designing the header section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
-import {Navbar} from "./components"
-import "./App.css"
-const App = ()=>{
-    return(
-        <div>
-        <Navbar/>
-        </div>
-    )
+import { Navbar } from "./components";
+import Home from "./pages/Home/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Router>
+        <Switch>
+          <Route component={Home} />
+        </Switch>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Typography, Button } from "@material-ui/core";
+import Phone from "../../assets/phone.png";
+import style from "./Home.module.css";
+const Home = () => {
+  return (
+    <div className={style.home}>
+      <div className={style.header}>
+        <div className={style.headerLeft}>
+          <Typography className={style.tagLine}>
+            Food delivery service
+          </Typography>
+          <Typography className={style.subtitle}>
+            Pick out for food and groceries online and have them delivered to
+            you in no time. Fetch is definitely the way to Go!  
+          </Typography>
+          <Button className={style.btn}>Get Started</Button>
+        </div>
+        <div className={style.headerRight}>
+          <img src={Phone} className={style.phone} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Home;
