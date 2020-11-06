2b1e5dd52aaa5c77d9371840de362b6f60ad209e
David Odesola <faithodesola@gmail.com>
2020-11-06 19:18:12 +0800

    Integrating a Phone number input to the Signup form

--- src/pages/Signin/Signin.js
+++ src/pages/Signin/Signin.js
@@ -1,24 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import { Typography, Button, TextField } from "@material-ui/core";
 import { Link } from "react-router-dom";
 import style from "./Signin.module.css";
 const Signin = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <div className={style.signin}>
       <div className={style.signinLeft}></div>
       <div className={style.signinRight}>
         <div className={style.container}>
-        <Typography className={style.title} gutterBottom align="center">Fetch</Typography>
-        <Typography className={style.signinText} align="center">Sign in to Fetch</Typography>
-          <Button className={style.googleBtn} variant="contained">Sign in with Google</Button>
-          <Typography color="textSecondary" variant="body2" align="center">Or</Typography>
-          <form>
-          <TextField className={style.input} label="Email" type="email" variant="outlined" size="small"/>
-          <TextField className={style.input} label="Password" type="password" variant="outlined" size="small"/>
-          <Button className={style.submit} type="submit" variant="contained" color="primary">Submit</Button>
-          <Typography variant="body2" align="center">Don't have an Account? 
-          <Link to="/signup" style={{textDecoration: "none"}}> Sign Up</Link>
+          <Typography className={style.title} gutterBottom align="center">
+            Fetch
           </Typography>
+          <Typography className={style.signinText} align="center">
+            Sign in to Fetch
+          </Typography>
+          <Button className={style.googleBtn} variant="contained">
+            Sign in with Google
+          </Button>
+          <Typography color="textSecondary" variant="body2" align="center">
+            Or
+          </Typography>
+          <form>
+            <TextField
+              className={style.input}
+              label="Email"
+              type="email"
+              variant="outlined"
+              size="small"
+              required
+              onChange={(e)=>setEmail(e.target.value)}
+            />
+            <TextField
+              className={style.input}
+              label="Password"
+              type="password"
+              variant="outlined"
+              size="small"
+              required
+              onChange={(e)=>setPassword(e.target.value)}
+            />
+            <Button
+              className={style.submit}
+              type="submit"
+              variant="contained"
+              color="primary"
+            >
+              Submit
+            </Button>
+            <Typography variant="body2" align="center">
+              Don't have an Account?
+              <Link to="/signup" style={{ textDecoration: "none" }}>
+                Sign Up
+              </Link>
+            </Typography>
           </form>
         </div>
       </div>
--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,28 +1,107 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Typography, Button, TextField } from "@material-ui/core";
 import { Link } from "react-router-dom";
+import axios from "axios";
 import style from "./Signup.module.css";
 const Signup = () => {
+  const [fname, setFname] = useState("");
+  const [lname, setLname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [number, setNumber] = useState("");
+  const [countryData, setCountryData] = useState({});
+  useEffect(() => {
+    const getCountryData = async () => {
+      // fetching country data (flag, calling code) from api
+      await axios("https://restcountries.eu/rest/v2/name/philippines").then(
+        (country) => {
+          const { flag, callingCodes } = country.data[0];
+          setCountryData({ flag, callingCodes});
+        }
+      );
+    };
+    getCountryData();
+  }, []);
   return (
     <div className={style.signup}>
       <div className={style.signupLeft}></div>
       <div className={style.signupRight}>
         <div className={style.container}>
-        <Typography className={style.title} gutterBottom align="center">Fetch</Typography>
-        <Typography className={style.signupText} align="center">Sign up to Fetch</Typography>
-          <Button className={style.googleBtn} variant="contained">Sign up with Google</Button>
-          <Typography color="textSecondary" variant="body2" align="center">Or</Typography>
-          <form>
-          <div className={style.name}>
-          <TextField className={style.input} label="First Name" type="text" variant="outlined" size="small"/>
-          <TextField className={style.input} label="Last Name" type="text" variant="outlined" size="small"/>
-          </div>
-          <TextField className={style.input} label="Email" type="email" variant="outlined" size="small"/>
-          <TextField className={style.input} label="Password" type="password" variant="outlined" size="small"/>
-          <Button className={style.submit} type="submit" variant="contained" color="primary">Create an Account</Button>
-          <Typography variant="body2" align="center">Already have an Account? 
-          <Link to="/signin" style={{textDecoration: "none"}}> Sign In</Link>
+          <Typography className={style.title} gutterBottom align="center">
+            Fetch
           </Typography>
+          <Typography className={style.signupText} align="center">
+            Sign up to Fetch
+          </Typography>
+          <Button className={style.googleBtn} variant="contained">
+            Sign up with Google
+          </Button>
+          <Typography color="textSecondary" variant="body2" align="center">
+            Or
+          </Typography>
+          <form>
+            <div className={style.name}>
+              <TextField
+                className={style.input}
+                label="First Name"
+                type="text"
+                variant="outlined"
+                size="small"
+              />
+              <TextField
+                className={style.input}
+                label="Last Name"
+                type="text"
+                variant="outlined"
+                size="small"
+              />
+            </div>
+            <TextField
+              className={style.input}
+              label="Email"
+              type="email"
+              variant="outlined"
+              size="small"
+            />
+            <div className={style.phone}>
+              <div className={style.code}>
+                <img
+                  className={style.flag}
+                  src={countryData?.flag}
+                  alt=""
+                />
+                <Typography>{countryData.callingCodes? ("+" + countryData.callingCodes[0]): ""}</Typography>
+              </div>
+              <TextField
+                className={style.input}
+                label="Phone Number"
+                type="text"
+                variant="outlined"
+                size="small"
+              />
+            </div>
+            <TextField
+              className={style.input}
+              label="Password"
+              type="password"
+              variant="outlined"
+              size="small"
+            />
+            <Button
+              className={style.submit}
+              type="submit"
+              variant="contained"
+              color="primary"
+            >
+              Create an Account
+            </Button>
+            <Typography variant="body2" align="center">
+              Already have an Account?
+              <Link to="/signin" style={{ textDecoration: "none" }}>
+                {" "}
+                Sign In
+              </Link>
+            </Typography>
           </form>
         </div>
       </div>
