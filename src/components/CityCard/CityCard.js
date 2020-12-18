abd27c246bccc238edd61afa6ad8446ad6bace17
David Odesola <faithodesola@gmail.com>
2020-12-19 02:00:53 +0800

    Designing the City Card

--- src/components/CityCard/CityCard.js
+++ src/components/CityCard/CityCard.js
@@ -1,11 +1,17 @@
 import React from "react";
 import style from "./CityCard.module.css";
-import { Typography } from "@material-ui/core";
+import Picture from "../../assets/Manila.jpeg";
+import { Typography, IconButton } from "@material-ui/core";
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 
 const CityCard = () => {
   return (
     <div className={style.cityCard}>
-      <Typography className={style.cityName}>Manila</Typography>
+      <div className={style.cityInfo}>
+        <Typography className={style.cityName}>Manila</Typography>
+        <IconButton size="medium" className={style.btn}><ArrowForwardIosIcon/></IconButton>
+      </div>
+      <img src={Picture} alt="Manila" className={style.picture} />
     </div>
   );
 };
