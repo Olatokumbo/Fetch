a2c62f1b26f74b572ab8081bb8befd3f4b1b1896
David Odesola <faithodesola@gmail.com>
2020-12-19 15:03:13 +0800

    Displaying the CityCards

--- src/components/CityCard/CityCard.js
+++ src/components/CityCard/CityCard.js
@@ -1,17 +1,17 @@
 import React from "react";
 import style from "./CityCard.module.css";
-import Picture from "../../assets/Manila.jpeg";
+// import Picture from "../../assets/Manila.jpeg";
 import { Typography, IconButton } from "@material-ui/core";
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 
-const CityCard = () => {
+const CityCard = ({data}) => {
   return (
     <div className={style.cityCard}>
       <div className={style.cityInfo}>
-        <Typography className={style.cityName}>Manila</Typography>
+        <Typography className={style.cityName}>{data.name}</Typography>
         <IconButton size="medium" className={style.btn}><ArrowForwardIosIcon/></IconButton>
       </div>
-      <img src={Picture} alt="Manila" className={style.picture} />
+      <img src={data.imageUrl} alt="Manila" className={style.picture} />
     </div>
   );
 };
--- src/containers/Cities/Cities.js
+++ src/containers/Cities/Cities.js
@@ -1,20 +1,45 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Typography } from "@material-ui/core";
 import CityCard from "../../components/CityCard/CityCard";
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { connect } from "react-redux";
+import * as actionCreator from "../../store/actions";
 import style from "./Cities.module.css";
 
-const Cities = () =>{
-    return(
-        <div className={style.cities}>
-            <Typography className={style.title}>Check out some our affiliated stores with these Cities</Typography>
-            <div className={style.wrapper}>
-            <div className={style.cityGrid}>
-                <CityCard/>
-                <CityCard/><CityCard/><CityCard/><CityCard/><CityCard/><CityCard/>
-            </div>
-            </div>
+const Cities = ({ getCities, cities }) => {
+  useEffect(() => {
+    getCities();
+  }, [getCities]);
+  return (
+    <div className={style.cities}>
+      <Typography className={style.title}>
+        Check out some our services within these Cities
+      </Typography>
+      <div className={style.wrapper}>
+        <div className={style.cityGrid}>
+          {cities.length >0 ?
+        (cities.map((data) => (
+            <CityCard key={data.id} data={data} />
+          ))): (<div className={style.loading}>
+            <CircularProgress />
+        </div>)
+        }
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Cities; 
+const mapStateToProps = (state) => {
+  return {
+    cities: state.city.cities,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getCities: () => dispatch(actionCreator.listCities()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Cities);
